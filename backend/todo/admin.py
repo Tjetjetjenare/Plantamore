84d6952df2352eb37484750363604fbebdc55e66
Max Norberg <mxnorberg5@gmail.com>
2022-04-07 11:13:50 +0200

    skapat databaserna
    data

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Todo
+from .models import Plant
+from .models import User
+from .models import Plant_subprofile
 
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
@@ -7,3 +10,6 @@ class TodoAdmin(admin.ModelAdmin):
 # Register your models here.
 
 admin.site.register(Todo, TodoAdmin)
+admin.site.register(Plant)
+admin.site.register(User)
+admin.site.register(Plant_subprofile)
--- backend/todo/models.py
+++ backend/todo/models.py
@@ -9,3 +9,31 @@ class Todo(models.Model):
 
     def _str_(self):
         return self.title
+
+class Plant(models.Model):
+    p_id = models.IntegerField(primary_key=True)
+    english_name = models.CharField(max_length=100)
+    swedish_name = models.CharField(max_length=100)
+    latin_name = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=2000)
+    nutrition = models.IntegerField()
+    sunlight = models.CharField(max_length=50)
+    water = models.CharField(max_length=50)
+    replant = models.IntegerField()
+    description = models.TextField()
+    family = models.CharField(max_length=100)
+
+class User(models.Model):
+    username = models.CharField(max_length=50, primary_key=True)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=50)
+
+class Plant_subprofile(models.Model):
+    sub_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    water = models.DateField()
+    replant = models.DateField()
+    nutrition = models.DateField()
+    p_id = models.ForeignKey('Plant', on_delete=models.CASCADE)
+    username = models.ForeignKey('User', on_delete=models.CASCADE)
