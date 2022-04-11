07d0d4b41435882033cd3538a9a361a7456f1523
PelleFre <pelle.freden99@gmail.com>
2022-04-11 14:15:21 +0200

    new commands

--- nativefront/App.js
+++ nativefront/App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View,Image, TouchableOpacity } from 'react-native';
 import axios from "axios"
 
 const plantbaseUrl = 'http://localhost:8000/api/students/';
 export default function App() {
-  
-  const getDataUsingAsyncAwaitGetCall = async () => {
+  const [State, setState] = useState("");
+  useEffect(async() => {
+    try {
+      const response = await axios.get(
+        plantbaseUrl,
+      );
+      setState(response.data[0]);
+    } catch (error) {
+      // handle error
+    }
+  })
+  /*const getDataUsingAsyncAwaitGetCall = async () => {
     try {
       const response = await axios.get(
         plantbaseUrl,
@@ -17,16 +27,27 @@ export default function App() {
       alert(error.message);
     }
   };
-  
-  return (
-    <View style={styles.container} >
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Here i want to display plant with id 1!</Text>
-      <TouchableOpacity
+
+
+  <TouchableOpacity
         style={styles.buttonStyle}
         onPress={getDataUsingAsyncAwaitGetCall}>
-        <Text>Click to see plant 1</Text>
+        
       </TouchableOpacity>
+  */
+  return (
+    <View style={styles.container} >
+      <Image
+        style={styles.tinyLogo}
+        source={State.image_url}
+      />
+      <Text>Name: {State.swedish_name}</Text>
+      <Text>Here i want to display plant with id 1!</Text>
+      <Text>Id: {State.p_id}</Text>
+      <Text>english_name: {State.english_name}</Text>
+      <Text>Latin_name: {State.latin_name}</Text>
+      <Text>description: {State.description}</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -40,5 +61,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 200,
+    height: 200,
   },
 });
