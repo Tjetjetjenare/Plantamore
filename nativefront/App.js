1805415c26c970759a0b6c1904bcb9ec657e0a3f
HannahLiljestam <93650279+HannahLiljestam@users.noreply.github.com>
2022-04-19 09:53:46 +0200

    symbols

--- nativefront/App.js
+++ nativefront/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View,Image, TouchableOpacity } from 'react-native';
 import axios from "axios"
 import Home from './screens/Home';
+import PlantDBProfile from './screens/PlantDBProfile';
 
 
 
@@ -10,7 +11,7 @@ const plantbaseUrl = 'http://localhost:8000/api/students/';
 
 export default function App() {
   
-  return <Home/>;
+  return <PlantDBProfile/>;
 
 }
 
