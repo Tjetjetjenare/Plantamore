332f734d595fd5cdd978748bef4145680fc4d6f5
PelleFre <pelle.freden99@gmail.com>
2022-04-20 12:14:33 +0200

    testing
    Nav

--- nativefront/App.js
+++ nativefront/App.js
@@ -7,98 +7,24 @@ import SignUp from './screens/SignUp';
 import LogIn from './screens/LogIn';
 import PlantDBProfile from './screens/PlantDBProfile';
 import PlantSubprofile from './screens/PlantSubprofile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 //import Calendar from './screens/Calendar';
 
-
-const plantbaseUrl = 'http://localhost:8000/api/plants/';
-const userbaseUrl = 'http://localhost:8000/api/users/';
- /*
+const Stack = createNativeStackNavigator();
 export default function App() {
- 
-  const [index, setIndex] = useState(0);
-  const [State, setState] = useState("");
   
-  useEffect(async() => {
-    try {
-      const response = await axios.get(
-        plantbaseUrl,
-      );
-      setState(response.data[index]);
-    } catch (error) {
-      // handle error
-    }
-  },[State])
+  return ( 
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name = "Home"
+          component={Home}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
 
-  /*function nextPlant() {
-    try {
-      const response =  axios.get(
-        plantbaseUrl,
-      );
-      var i = index +1;
-      setIndex(i)
-      console.log("SWITCH")
-      setState(response.data[index]);
-    } catch (error) {
-      console.log(error)
-      // handle error
-    }
-  }*/
-
-   
-  /*const getDataUsingAsyncAwaitGetCall = async () => {
-    try {
-      const response = await axios.get(
-        plantbaseUrl,
-      );
-      alert(JSON.stringify(response.data[0]));
-    } catch (error) {
-      // handle error
-      alert(error.message);
-    }
-  };
-
-
-  <TouchableOpacity
-        style={styles.buttonStyle}
-        onPress={getDataUsingAsyncAwaitGetCall}>
-        
-      </TouchableOpacity>
-  
-  return (
-    <View style={styles.container} >
-      <Image
-        style={styles.tinyLogo}
-       // source={State.image_url}
-      />
-      <Text>Name: {State.email}</Text>
-      <Text>Here i want to display plant with id 1!</Text>
-      <Text>english_name: {State.english_name}</Text>
-      <Text>Latin_name: {State.password}</Text>
-      <Text>description:</Text>
-      <Button
-        onPress={() => setIndex(1)}
-        color="#841584"
-      />
-      <StatusBar style="auto" />
-    </View>
   );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  tinyLogo: {
-    width: 200,
-    height: 200,
-  },
-});
-*/
-export default function App() {
-  
-  return <LogIn/>;
 
 }
 
