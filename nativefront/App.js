9f654cb20f8f4a2fc6ce22127aabf071637e816f
whatmat <elaf.salam.6921@student.uu.se>
2022-04-24 14:45:30 +0200

    tempo

--- nativefront/App.js
+++ nativefront/App.js
@@ -9,24 +9,28 @@ import PlantDBProfile from './screens/PlantDBProfile';
 import PlantSubprofile from './screens/PlantSubprofile';
 import Profile from './screens/Profile';
 import Calendar from './screens/Calendar';
-import CreatePlantSubprofile from './screens/CreatePlantSubprofile';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Watered from './screens/Watered';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import CustomDrawer from './components/CustomDrawer';
+
 const plantbaseUrl = 'http://localhost:8000/api/plants/';
 const userbaseUrl = 'http://localhost:8000/api/users/';
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-  
 
-  return ( 
-    <NavigationContainer>
-      <Stack.Navigator
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  <NavigationContainer>
+    <Stack.Navigator
       screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen
+      headerShown: false
+    }}>
+      <Stack.Screen
           name = "Home"
           component={Home}
           />
@@ -37,10 +41,6 @@ export default function App() {
           <Stack.Screen
           name = "SignUp"
           component={SignUp}
-          />
-          <Stack.Screen
-          name = "PlantDB"
-          component={PlantDBProfile}
           />
           <Stack.Screen
           name = "PlantSub"
@@ -54,8 +54,104 @@ export default function App() {
           name = "Profile"
           component={Profile}
           />
+        <Stack.Screen
+          name = "PlantDB"
+          component={PlantDBProfile}
+          />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-}
 
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator drawerContent ={ props => <CustomDrawer {...props} />}
+        screenOptions={{
+          headerTintColor: '#000',
+          drawerActiveBackgroundColor: '#FFF',
+          drawerActiveTintColor: '#000',
+          drawerLabelStyle: {
+            color: '#000',
+            fontSize: 15
+          },
+          drawerStyle: {
+          backgroundColor: 'transparent',
+          borderRadius:20,
+          width:240
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="Plant Cat 1"
+          component={PlantDBProfile}
+          options={{
+            title:'Plant Cat 1',
+            headerStyle: {
+              backgroundColor: '#FFF'
+            },
+            drawerIcon: () => (
+            <Ionicons name="leaf-outline" size={25} color={'#000'} />
+          )}}
+        />
+        <Drawer.Screen
+          name="Plant Cat 2"
+          component={PlantDBProfile}
+          options={{
+            title:'Plant Cat 2',
+            headerStyle: {
+              backgroundColor: '#FFF'
+            },
+            drawerIcon: () => (
+            <Ionicons name="leaf-outline" size={25} color={'#000'} />
+          )}}
+        />
+        <Drawer.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title:'Home',
+            headerStyle: {
+              backgroundColor: '#FFF'
+            },
+            drawerIcon: () => (
+            <Ionicons name="home-outline" size={25} color={'#000'} />
+          )}}
+        />
+        <Drawer.Screen
+          name="Settings"
+          component={Home}
+          options={{
+            title:'Settings',
+            headerStyle: {
+              backgroundColor: '#FFF'
+            },
+            drawerIcon: () => (
+            <Ionicons name="settings-outline" size={25} color={'#000'} />
+          )}}
+        />
+        <Drawer.Screen
+          name="Get Help"
+          component={Home}
+          options={{
+            title:'Get Help',
+            headerStyle: {
+              backgroundColor: '#FFF'
+            },
+            drawerIcon: () => (
+            <Ionicons name="help-circle-outline" size={25} color={'#000'} />
+          )}}
+        />
+        <Drawer.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            title:'Profile',
+            headerStyle: {
+              backgroundColor: '#7E9B6D'
+            },
+            drawerIcon: () => (
+            <Ionicons name="person-outline" size={25} color={'#000'} />
+          )}}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
