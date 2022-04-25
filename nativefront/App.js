18fd3a361edb214d087ef48bbe2298d879e97038
whatmat <elaf.salam.6921@student.uu.se>
2022-04-25 09:27:10 +0200

    plantor i databasen

--- nativefront/App.js
+++ nativefront/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View,Image,Button, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, Component } from "react";
+import { StyleSheet, Text, View,Image,Button, TouchableOpacity, ScrollView, Switch } from 'react-native';
 import axios from "axios"
 import Home from './screens/Home';
 import SignUp from './screens/SignUp';
@@ -20,11 +20,212 @@ const plantbaseUrl = 'http://localhost:8000/api/plants/';
 const userbaseUrl = 'http://localhost:8000/api/users/';
 const Stack = createNativeStackNavigator();
 
-
-
 const Drawer = createDrawerNavigator();
 
+
+
+
+// import Constants from 'expo-constants';
+// import * as Animatable from 'react-native-animatable';
+// import Collapsible from 'react-native-collapsible';
+// import Accordion from 'react-native-collapsible/Accordion';
+
+// const BACON_IPSUM =
+//   'Hello ';
+
+// const CONTENT = [
+//   {
+//     title: 'First',
+//     content: BACON_IPSUM,
+//   },
+//   {
+//     title: 'Second',
+//     content: BACON_IPSUM,
+//   },
+//   {
+//     title: 'Third',
+//     content: BACON_IPSUM,
+//   },
+//   {
+//     title: 'Fourth',
+//     content: BACON_IPSUM,
+//   },
+//   {
+//     title: 'Fifth',
+//     content: BACON_IPSUM,
+//   },
+// ];
+
+
+// export default class App extends Component {
+//   state = {
+//     activeSections: [],
+//     collapsed: true,
+//     multipleSelect: false,
+//   };
+
+//   toggleExpanded = () => {
+//     this.setState({ collapsed: !this.state.collapsed });
+//   };
+
+//   setSections = (sections) => {
+//     this.setState({
+//       activeSections: sections.includes(undefined) ? [] : sections,
+//     });
+//   };
+
+//   renderHeader = (section, _, isActive) => {
+//     return (
+//       <Animatable.View
+//         duration={400}
+//         style={[styles.header, isActive ? styles.active : styles.inactive]}
+//         transition="backgroundColor"
+//       >
+//         <Text style={styles.headerText}>{section.title}</Text>
+//       </Animatable.View>
+//     );
+//   };
+
+//   renderContent(section, _, isActive) {
+//     return (
+//       <Animatable.View
+//         duration={400}
+//         style={[styles.content, isActive ? styles.active : styles.inactive]}
+//         transition="backgroundColor"
+//       >
+//         <Animatable.Text animation={isActive ? 'bounceIn' : undefined}>
+//           {section.content}
+//         </Animatable.Text>
+//       </Animatable.View>
+//     );
+//   }
+
+//   render() {
+//     const { multipleSelect, activeSections } = this.state;
+
+//     return (
+//       <View style={styles.container}>
+//         <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
+//           <Text style={styles.title}>Accordion Example</Text>
+
+//           <View style={styles.multipleToggle}>
+//             <Text style={styles.multipleToggle__title}>Multiple Select?</Text>
+//             <Switch
+//               value={multipleSelect}
+//               onValueChange={(a) => this.setState({ multipleSelect: a })}
+//             />
+//           </View>
+
+//           <TouchableOpacity onPress={this.toggleExpanded}>
+//             <View style={styles.header}>
+//               <Text style={styles.headerText}>Single Collapsible</Text>
+//             </View>
+//           </TouchableOpacity>
+//           <Collapsible collapsed={this.state.collapsed} align="center">
+//             <View style={styles.content}>
+//               <Text>
+//                 Single Collapsible
+//               </Text>
+//             </View>
+//           </Collapsible>
+//           <Accordion
+//             activeSections={activeSections}
+//             sections={CONTENT}
+//             touchableComponent={TouchableOpacity}
+//             expandMultiple={multipleSelect}
+//             renderHeader={this.renderHeader}
+//             renderContent={this.renderContent}
+//             duration={400}
+//             onChange={this.setSections}
+//             renderAsFlatList={false}
+//           />
+//         </ScrollView>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#F5FCFF',
+//     paddingTop: 55,
+//   },
+//   title: {
+//     textAlign: 'center',
+//     fontSize: 22,
+//     fontWeight: '300',
+//     marginBottom: 20,
+//   },
+//   header: {
+//     backgroundColor: '#F5FCFF',
+//     padding: 10,
+//   },
+//   headerText: {
+//     textAlign: 'center',
+//     fontSize: 16,
+//     fontWeight: '500',
+//   },
+//   content: {
+//     padding: 20,
+//     backgroundColor: '#fff',
+//   },
+//   active: {
+//     backgroundColor: 'rgba(255,255,255,1)',
+//   },
+//   inactive: {
+//     backgroundColor: 'rgba(245,252,255,1)',
+//   },
+//   selectors: {
+//     marginBottom: 10,
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//   },
+//   selector: {
+//     backgroundColor: '#F5FCFF',
+//     padding: 10,
+//   },
+//   activeSelector: {
+//     fontWeight: 'bold',
+//   },
+//   selectTitle: {
+//     fontSize: 14,
+//     fontWeight: '500',
+//     padding: 10,
+//   },
+//   multipleToggle: {
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     marginVertical: 30,
+//     alignItems: 'center',
+//   },
+//   multipleToggle__title: {
+//     fontSize: 16,
+//     marginRight: 8,
+//   },
+// });
+
+const CONTENT = [
+  {
+  isExpanded: false,
+  category_name: 'Item 1',
+  subcategory: [
+    {id:1, val:'Sub 1'},
+    {id:2, val:'Sub 2'}
+    ]
+  },
+  {
+    isExpanded: false,
+    category_name: 'Item 2',
+    subcategory: [
+      {id:3, val:'Sub 4'},
+      {id:4, val:'Sub 5'}
+      ]
+    },
+];
+
 export default function App() {
+  const [multiSelect, setmultiSelect] = useState(false);
   <NavigationContainer>
     <Stack.Navigator
       screenOptions={{
