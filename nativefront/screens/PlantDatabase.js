178c411d9d2269f5c0ff84cd31797449dfd2a925
HannahLiljestam <93650279+HannahLiljestam@users.noreply.github.com>
2022-05-03 15:36:53 +0200

    bla bla bla

--- nativefront/screens/PlantDatabase.js
+++ nativefront/screens/PlantDatabase.js
@@ -1,14 +1,69 @@
-import {React, useState} from 'react';
-import {View, TouchableOpacity, Text, SafeAreaView, StyleSheet, Image, Alert} from 'react-native';
+import {React, useState, useEffect} from 'react';
+import axios from 'axios';
+import {View, TouchableOpacity, ItemSeparatorView, ItemView, FlatList, Text, SafeAreaView, StyleSheet, Image, Alert} from 'react-native';
+
+var plantbaseUrl = null;
+
+if(Platform.OS === "android"){ plantbaseUrl = 'http://10.0.2.2:8000/api/plants/';}
+else{  plantbaseUrl = 'http://127.0.0.1:8000/api/plants/';}
 
 
+// function PlantDatabase(props) {
+//     const [filteredDataSource, setFilteredDataSource] = useState([]);
+//     const [masterDataSource, setMasterDataSource] = useState([]);
 
+//     useEffect(async() => {
+//         try {
+//           const response = await axios.get(
+//            plantbaseUrl,
+//           );
+//           setFilteredDataSource(response.data);
+//           setMasterDataSource(response.data);
+          
+//         } catch (error) {
+//           console.error(error);
+//         }
+//       },[]);
 
-function PlantDatabase(props) {
+function PlantDatabase({navigation}) {
+    const [Plant, setPlant] = useState("");
+
+    
+  useEffect(async() => {
+    try {
+      const response = await axios.get(
+       plantbaseUrl,
+      );
+      setPlant(response.data);
+    } catch (error) {
+    }
+  },[]);
+
+  const Item = ({ english_name }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{english_name}</Text>
+    </View>
+  );
+
+  const App = () => {
+    const renderItem = ({ item }) => (
+      <Item english_name={item.english_name} />
+    );
+  }
+
     return (
         <SafeAreaView style={styles.background}>
             <Text style={styles.title}>Plant database</Text>
-        
+            
+
+        <View style = {styles.scroll}>
+        <FlatList
+            data={Plant}
+            keyExtractor={(item, index) => index.toString()}
+            ItemSeparatorComponent={ItemSeparatorView}
+            renderItem={ItemView}
+            />
+        </View>
         </SafeAreaView>
     );
 }
