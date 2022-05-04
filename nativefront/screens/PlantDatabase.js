26071936f7b6c5d48cb8a67d50af786890741521
whatmat <elaf.salam.6921@student.uu.se>
2022-05-04 20:52:10 +0200

    idk

--- nativefront/screens/PlantDatabase.js
+++ nativefront/screens/PlantDatabase.js
@@ -6,8 +6,27 @@ var plantbaseUrl = null;
 
 if(Platform.OS === "android"){ plantbaseUrl = 'http://10.0.2.2:8000/api/plants/';}
 else{  plantbaseUrl = 'http://127.0.0.1:8000/api/plants/';}
+<<<<<<< HEAD
 
 const engNameList = [];
+=======
+const engNameList = [];
+function PlantDatabase({navigation}) {
+  const [Plant, setPlant] = useState("");
+  useEffect(async() => {
+    try {
+      const response = await axios.get(
+       plantbaseUrl,
+      );
+      setPlant(response.data);
+      for (var i=0;i<response.data.length;i++){
+        engNameList.push(response.data[i].english_name)
+      }
+    } catch (error) {
+    }
+    console.log(engNameList)
+  },[]);
+>>>>>>> 32d8853beb04ff95c3d944766c5fed6f38c56ebc
 
 function PlantDatabase() {
   const [plant, setPlants] = useState("");
