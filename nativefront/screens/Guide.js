ead0d045a39f6faca8d5ee006ef0448d91e8d04f
motmarsda <93783929+motmarsda@users.noreply.github.com>
2022-05-04 15:49:24 +0200

    guide screen

--- nativefront/screens/Guide.js
+++ nativefront/screens/Guide.js
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, StatusBar, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, StatusBar, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Ionicons } from "@expo/vector-icons";
+import * as guideText from '../assets/guideText.json'
 
 
 export default function Guide({navigation}) {
+
   return (
     <SafeAreaView style={styles.container}>
         <View style={styles.symbols}>
@@ -16,15 +18,38 @@ export default function Guide({navigation}) {
               />
           </TouchableOpacity>
       </View>
+      <StatusBar style="auto"/>
       <ScrollView>
-        <StatusBar style="auto"/>
-        <Text style={styles.header}>Guide</Text>
-        <Text style={styles.headline}>How to use PLANTAMORE</Text>
-        <Text style={styles.headline}>Terminology</Text>
-        <Text style={styles.headline}>Disclaimer</Text>
-        <Text style={styles.body}>
-          This application is ment to help 
-        </Text>
+        <Text style={styles.header}>Your guide to</Text>
+        <View style={styles.imgwrap}>
+           <Image style={styles.logo} source={require("../assets/logo.png")}></Image>
+        </View>
+        <View style={styles.pt1}>
+          <Text style={styles.headline1}>About PLANTAMORE</Text>
+          <Text style={styles.body}>{guideText.about}</Text>      
+        </View>
+        <View style={styles.pt2}>
+          <Text style={styles.headline1}>Terminology</Text>
+          <Text style={styles.body}>{guideText.terms}</Text>
+          <Text style={styles.headline2}>Water:</Text>
+          <Text style={styles.body}>{guideText.water3}</Text>
+          <Text style={styles.body}>{guideText.water2}</Text>
+          <Text style={styles.body}>{guideText.water1}</Text>
+          <Text style={styles.headline2}>Nutrition:</Text>
+          <Text style={styles.body}>{guideText.nutri3}</Text>
+          <Text style={styles.body}>{guideText.nutri2}</Text>
+          <Text style={styles.body}>{guideText.nutri1}</Text>      
+          <Text style={styles.headline2}>Sun:</Text>
+          <Text style={styles.body}>{guideText.sun5}</Text>
+          <Text style={styles.body}>{guideText.sun4}</Text>
+          <Text style={styles.body}>{guideText.sun3}</Text>
+          <Text style={styles.body}>{guideText.sun2}</Text>
+          <Text style={styles.body}>{guideText.sun1}</Text> 
+        </View>
+        <View style={styles.pt3}>
+          <Text style={styles.headline1}>Disclaimer</Text>
+          <Text style={styles.body}>{guideText.disc}</Text>
+        </View>
         </ScrollView>
     </SafeAreaView>
   )
@@ -38,17 +63,51 @@ const styles = StyleSheet.create({
   header: { 
     fontSize: 35, 
     fontWeight: 'bold', 
-    marginTop: 20, 
-    marginBottom: 20,
+    marginTop: 30, 
     textAlign:"center",
   },
-  headline:{
+  headline1:{
     fontSize: 18,
-    marginTop: 10,
     fontWeight: 'bold',
+  },
+  headline2:{
+    fontSize: 16,
+    marginTop: 5,
+    fontWeight: 'bold',
+  },
+  body:{
+    fontSize: 13,
   },
   symbols: {
     flexDirection: 'row',
-},
+  },
+  imgwrap:{
+    width: "80%", 
+    height: 140,
+    borderRadius:20,
+    backgroundColor:"#fff",
+    alignSelf: 'center',
+    alignItems:'center',
+    justifyContent:'center',  
+    paddingRight: 20,
+    marginTop:30,
+    marginBottom:30,
+  },
+  logo:{
+    height: "75%",
+    resizeMode: "contain",
+  },
+  pt1:{
+    backgroundColor: '#7E9B6D',
+    padding:15,
+  },
+  pt2:{
+    backgroundColor: '#95b881',
+    padding:15,
+  },
+  pt3:{
+    backgroundColor: '#a7cf91',
+    padding:15,
+  },
 
 })
