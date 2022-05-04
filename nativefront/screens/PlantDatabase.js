7cdf0006aa0734a6ab13a32ff5a1c910147f8bb0
whatmat <elaf.salam.6921@student.uu.se>
2022-05-04 20:58:14 +0200

    idk

--- nativefront/screens/PlantDatabase.js
+++ nativefront/screens/PlantDatabase.js
@@ -8,21 +8,6 @@ if(Platform.OS === "android"){ plantbaseUrl = 'http://10.0.2.2:8000/api/plants/'
 else{  plantbaseUrl = 'http://127.0.0.1:8000/api/plants/';}
 
 const engNameList = [];
-function PlantDatabase({navigation}) {
-  const [Plant, setPlant] = useState("");
-  useEffect(async() => {
-    try {
-      const response = await axios.get(
-       plantbaseUrl,
-      );
-      setPlant(response.data);
-      for (var i=0;i<response.data.length;i++){
-        engNameList.push(response.data[i].english_name)
-      }
-    } catch (error) {
-    }
-    console.log(engNameList)
-  },[]);
 
 function PlantDatabase() {
   const [plant, setPlants] = useState("");
