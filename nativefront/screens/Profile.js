afba74991638a368c0f56f5e24603bf31cc19cf9
whatmat <elaf.salam.6921@student.uu.se>
2022-05-12 12:24:46 +0200

    flatlist

--- nativefront/screens/Profile.js
+++ nativefront/screens/Profile.js
@@ -80,6 +80,56 @@ const Item = ({ id, name, birth_date, water,replant,nutrition,p_id,username, pla
         </View>
     </TouchableOpacity>
   );}}
+
+//   const DATA = [
+//     {
+//       id: "1",
+//       renderItem: renderImg,
+//       data: require('../assets/profileTest.png'),
+//     },
+//   ];
+
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+  
+
+  const renderImg = () => {
+    if(pImage!='false'){
+      return(            
+          <Image
+              style={styles.plantPic}
+              source={require("../assets/profilePic.png")}
+          />
+      )         
+    }else{
+      return(            
+          <Image
+              style={styles.plantPic}
+              source={require("../assets/onlyPlantSmall.png")}
+          />
+      )}
+  };
+
+
+  const ItemPic = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+
+
 function findMyPlants(userPlants, username){
     myPlants.length = 0
     for ( var i = 0; i< userPlants.length; i++){
@@ -100,6 +150,8 @@ function Profile({navigation}) {
     const [plants, setPlants] = useState({});
     const [username, setUsername] = useState("");
     const isFocused = useIsFocused();
+    const [pImage, setPImage] = useState('true');
+
     useEffect(async() => {
         AsyncStorage.getItem('MyName').then(value =>
             //AsyncStorage returns a promise so adding a callback to get the value
@@ -121,6 +173,7 @@ function Profile({navigation}) {
           // handle error
         }
       },[isFocused,myPlants]);
+
     const renderItem = ({ item }) => (
         <Item 
             id = {item.sub_id}
@@ -135,15 +188,29 @@ function Profile({navigation}) {
             navigation = {navigation}
             
             /> );
+
+            const renderPic = ({ item }) => (
+                <ItemPic title={item.title} />
+              );
   
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.symbols}>
             </View>
             <Text style={styles.userName}>{username}</Text>
-            <Image 
-                style={styles.profilePic}
-                source={require("../assets/profilePic.png")}></Image>
+            <View>
+            <TouchableOpacity onPress={() => renderPic(item)}>   
+                <Image 
+                    style={styles.profilePic}
+                    source={require("../assets/profilePic.png")}>
+                </Image>
+            </TouchableOpacity>
+                    <FlatList
+                    data={DATA}
+                    renderItem={renderPic}
+                    keyExtractor={item => item.id}
+                    />
+            </View>
             <View style={styles.scrollView}
                   contentContainerStyle={{flexDirection:'row'}}>
                 <FlatList 
@@ -158,9 +225,7 @@ function Profile({navigation}) {
 
             <TouchableOpacity 
                 style={styles.circle}
-                onPress={() => navigation.navigate('Watered') 
-            
-                
+                onPress={() => navigation.navigate('Watered')        
                 }>
                 <Image style={styles.wateringCan}
                         source={require("../assets/plantCare.png")}>  
