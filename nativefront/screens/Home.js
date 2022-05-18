7bfb151db3fce78417363aa7a085af68d02f2698
Max Norberg <mxnorberg5@gmail.com>
2022-05-18 10:30:22 +0200

    Städning i Home och LogIn

--- nativefront/screens/Home.js
+++ nativefront/screens/Home.js
@@ -8,97 +8,86 @@ import { useIsFocused } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 
 var plantbaseUrl = null;
-
 if(Platform.OS === "android"){ plantbaseUrl = 'http://10.0.2.2:8000/api/plants/';}
 else{  plantbaseUrl = 'http://127.0.0.1:8000/api/plants/';}
 
-
-
 function Home({navigation}) {
-    const [filteredDataSource, setFilteredDataSource] = useState([]);
-    const [masterDataSource, setMasterDataSource] = useState([]);
-    const [search, setSearch] = useState('');
-    const [logd, setLogd] = useState('');
-    const isFocused = useIsFocused();
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+  const [search, setSearch] = useState('');
+  const [logd, setLogd] = useState('');
+  const isFocused = useIsFocused();
   useEffect(async() => {
-    if(isFocused){         console.log("called");
-    AsyncStorage.getItem('inloggad').then(value =>
-       setLogd(value )
-  );
-  console.log(logd)
+    if(isFocused){
+      AsyncStorage.getItem('inloggad').then(value =>
+        setLogd(value )
+      );
     }
     try {
       const response = await axios.get(
-       plantbaseUrl,
+        plantbaseUrl,
       );
       setFilteredDataSource(response.data);
       setMasterDataSource(response.data);
-      
-    } catch (error) {
+    }
+    catch (error) {
       console.error(error);
     }
   },[logd,isFocused]);
-  
   const SearchField = () => {
     return (
-        <View style={styles.container2}>
-            <TextInput 
-                style={styles.bar} 
-                placeholder='Search' 
-                onChangeText={(text) => searchFilterFunction(text)}
-                value= {search}
-                placeholderTextColor={"white"}/>
-                
-        </View>
+      <View style={styles.container2}>
+        <TextInput 
+          style={styles.bar} 
+          placeholder='Search' 
+          onChangeText={(text) => searchFilterFunction(text)}
+          value= {search}
+          placeholderTextColor={"white"}
+        />      
+      </View>
     )
   }
-
   const SearchList = () => {
     if (search.length < 1){
       return
     }
     else{
-    return(
-      <View style = {styles.scroll}>
-        <FlatList
-          data={filteredDataSource}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={ItemSeparatorView}
-          renderItem={ItemView}
-        />
-      </View>
-    )
-  }};
+      return(
+        <View style = {styles.scroll}>
+          <FlatList
+            data={filteredDataSource}
+            keyExtractor={(item, index) => index.toString()}
+            ItemSeparatorComponent={ItemSeparatorView}
+            renderItem={ItemView}
+          />
+        </View>
+      )
+    }
+  };
   const ButtonWrap = () => {
     if (logd == 'false'){
       return(
         <View style={styles.buttonWrapper}>
-            <View style={styles.loginWrap}>
-              <StandardButton sizeFont={20} title="Log in" functionOnPress={() => navigation.navigate('ProfileDrawer', {screen: 'LogIn'})} />
-            </View>
-            <View style={styles.loginWrap}>
-              <StandardButton sizeFont={20} title="Sign up" functionOnPress={() => navigation.navigate('ProfileDrawer', {screen:'SignUp'})}/>
-            </View>
-          
+          <View style={styles.loginWrap}>
+            <StandardButton sizeFont={20} title="Log in" functionOnPress={() => navigation.navigate('ProfileDrawer', {screen: 'LogIn'})} />
+          </View>
+          <View style={styles.loginWrap}>
+            <StandardButton sizeFont={20} title="Sign up" functionOnPress={() => navigation.navigate('ProfileDrawer', {screen:'SignUp'})}/>
+          </View>
         </View>
       )
-    } else{
+    }
+    else{
       return(
         <TouchableOpacity onPress={() => navigation.navigate('GuideMain')} style={styles.careGuideOPA}>
           <Ionicons style={styles.careGuideIcon} name="information-circle-outline" size={28} color={'#FFF'} />
           <Text style={styles.careGuideButton}>Care Guide</Text>
         </TouchableOpacity>
-    )}
-
-
+      )
+    }
   }
-
   const searchFilterFunction = (text) => {
-    // Check if searched text is not blank
     if (text) {
-      // Inserted text is not blank
-      // Filter the masterDataSource
-      // Update FilteredDataSource
       const newData = masterDataSource.filter(function (item) {
         const itemData = item.english_name
           ? item.english_name.toUpperCase()
@@ -108,28 +97,23 @@ function Home({navigation}) {
       });
       setFilteredDataSource(newData);
       setSearch(text);
-    } else {
-      // Inserted text is blank
-      // Update FilteredDataSource with masterDataSource
+    }
+    else {
       setFilteredDataSource(masterDataSource);
       setSearch(text);
     }
   };
-
   const ItemView = ({ item }) => {
     return (
-      // Flat List Item
       <View style={styles.item}>
-      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-        {item.english_name.toUpperCase()}
-      </Text>
+        <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+          {item.english_name.toUpperCase()}
+        </Text>
       </View>
     );
   };
-
   const ItemSeparatorView = () => {
     return (
-      // Flat List Item Separator
       <View
         style={{
           height: 0.5,
@@ -139,54 +123,27 @@ function Home({navigation}) {
       />
     );
   };
-
   const getItem = (item) => {
-    // Function for click on an item
-    navigation.navigate("PlantDBHomePath", {plantId : item.p_id, EnglishName : item.english_name, LatinName : item.latin_name,
-    SwedishName: item.swedish_name,Description: item.description, Sun : item.sunlight, Water: item.water,
+    navigation.navigate("PlantDBHomePath", {plantId: item.p_id, EnglishName: item.english_name, LatinName: item.latin_name,
+    SwedishName: item.swedish_name,Description: item.description, Sun: item.sunlight, Water: item.water,
     Nutrition: item.nutrition, ImageUrl: item.image_url});
   };
-  
   return (
-    
     <SafeAreaView style={styles.container}>
-        <StatusBar style="auto"/>
-        <View style={styles.greenAccent} />
-      <Image style={styles.logo} source={require("../assets/logo.png")}></Image>
+      <StatusBar style="auto"/>
+      <View style={styles.greenAccent} />
+      <Image style={styles.logo} source={require("../assets/logo.png")}/>
       <View style= {styles.searchContainer}>
         {SearchField()}
         {SearchList()}
-        
       </View>
       {ButtonWrap()}
     </SafeAreaView>
   );
 }
-
 export default Home;
 
 const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    width: "80%",
-    resizeMode: "contain",
-    position:"absolute",
-    top: 0,
-  },
-  container2: {
-    width:"90%",
-    height:50,
-    opacity: 1,
-  },
-  container3: {
-    flex:1,
-    backgroundColor: '#fff',
-  },
   bar: {
     width: "100%",
     height: "100%",
@@ -196,27 +153,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     textAlign: "center",
     opacity: 0.4,
-    position:'absolute',
+    position: 'absolute',
     top: 20,
-  },
-  buttonWrapper: {
-    alignItems: "center",
-    position:"absolute",
-    top: 390, 
-  },
-  greenAccent:{
-    position: "absolute",
-    top: 312,
-    bottom:-100,
-    width: "300%",
-    height: "100%",
-    backgroundColor: "#7E9B6D",
-    transform: [{rotate: '-30deg'}]
-  }, 
-  loginWrap: {
-    width: "200%",
-    height: 40,
-    marginTop: 20,
   },
   burgerMenu: {
     height: 30,
@@ -224,6 +162,71 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 20,
     left: 20,
+  },
+  buttonWrapper: {
+    alignItems: "center",
+    position: "absolute",
+    top: 390, 
+  },
+  careGuideButton: {
+    textDecorationLine: 'underline',
+    fontSize: 27, 
+    color: '#fff',
+    alignSelf: 'center',
+  },
+  careGuideIcon: {
+    top: 35,
+    left: -30,
+  },
+  careGuideOPA: {
+    textDecorationLine: 'underline',
+    fontSize: 23,
+    top: 180,
+  },
+  container: {
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    width: "90%",
+    height: 50,
+    opacity: 1,
+  },
+  container3: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  greenAccent: {
+    position: "absolute",
+    top: 312,
+    bottom: -100,
+    width: "300%",
+    height: "100%",
+    backgroundColor: "#7E9B6D",
+    transform: [{rotate: '-30deg'}],
+  },
+  item: {
+    padding: 5,
+    marginHorizontal: 10,
+    opacity: 2,
+  },
+  itemStyle: {
+    padding: 5,
+    margin: 5,
+    color: 'white'
+  },
+  loginWrap: {
+    width: "200%",
+    height: 40,
+    marginTop: 20,
+  },
+  logo: {
+    width: "80%",
+    resizeMode: "contain",
+    position: "absolute",
+    top: 0,
   },
   profileButton: {
     height: 30,
@@ -233,48 +236,20 @@ const styles = StyleSheet.create({
     right: 20,
   },
   scroll:{
-    height:"280%",
-    width:"90%",
-    marginTop:51,
-    // backgroundColor: "black",
-
+    height: "280%",
+    width: "90%",
+    marginTop: 51,
     borderRadius: 20,
     textAlign: "center",
     opacity: 1,
-    position:'absolute',
-    top:20,
+    position: 'absolute',
+    top: 20,
     backgroundColor: 'gray',
   },
-  searchContainer:{
-    width:"95%",
-    alignItems:'center',
-    position:'absolute',
+  searchContainer: {
+    width: "95%",
+    alignItems: 'center',
+    position: 'absolute',
     top: 200,
-  },
-  item: {
-    padding: 5,
-    marginHorizontal: 10,
-    opacity: 2,
-  },
-  itemStyle: {
-    padding: 5,
-    margin:5,
-    color: 'white'
-  },
-  careGuideButton: {
-    textDecorationLine: 'underline',
-    fontSize: 27, 
-    color: '#fff',
-    alignSelf: 'center',
-    
-  },
-  careGuideOPA: {
-    textDecorationLine: 'underline',
-    fontSize: 23,
-    top: 180,
-  },
-  careGuideIcon: {
-    top: 35,
-    left: -30,
   },
 })
--- nativefront/screens/LogIn.js
+++ nativefront/screens/LogIn.js
@@ -6,8 +6,8 @@ import axios from "axios";
 import {TextInput} from 'react-native-paper'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useIsFocused } from "@react-navigation/native";
-var userbaseUrl = null;
 
+var userbaseUrl = null;
 if(Platform.OS === "android"){ userbaseUrl = 'http://10.0.2.2:8000/api/users/';}
 else{  userbaseUrl = 'http://127.0.0.1:8000/api/users/';}
 
@@ -18,20 +18,17 @@ function LogInScreen({navigation}) {
     const [existingUsers, setExistingUsers] = useState("");
     const [isSecurePassword, setIsSecurePassword] = useState(true);
     const isFocused = useIsFocused();
-    
-
     const onChangeEmailHandler = (email) => {
         setEmail(email);
     };
     const onChangePasswordHandler = (password) => {
         setPassword(password);
     };
-    
     const onSubmitFormHandler =  (async) => {
         let max = existingUsers.length;
         if (!email.trim() || !password.trim()) {
             Alert.alert("Error","Email or password is invalid. Please try again.");
-           return;
+            return;
         }
         setIsLoading(true);
         try {
@@ -40,10 +37,8 @@ function LogInScreen({navigation}) {
                     if(existingUsers[i].password == password){
                         AsyncStorage.setItem("inloggad", "true");
                         AsyncStorage.setItem("propic", ""+ existingUsers[i].profile_picture);
-                        console.log(""+ existingUsers[i].profile_picture)
                         AsyncStorage.setItem("MyName", existingUsers[i].username);
                         setIsLoading(false);
-
                         navigation.navigate('ProfileDrawer', {screen: 'Profile'})
                         return;
                     }
@@ -56,71 +51,67 @@ function LogInScreen({navigation}) {
             }
             Alert.alert("Error","There is no user with these credentials");
             setIsLoading(false);
-        } catch (error) {
+        }
+        catch (error) {
             Alert.alert("Error","An error has occurred, please try again");
-            console.log("email: ", email ,"pass: ", password)
             setIsLoading(false);
         }
-        };
-        useEffect(async() => {
-            try {
-              const response = await axios.get(
-               userbaseUrl,
-              );
-              setExistingUsers(response.data);
-            } catch (error) {
-              // handle error
-            }
-          },[isFocused]);
-          const imageClick = () => {
-            console.log('Click');
-          } 
+    };
+    useEffect(async() => {
+        try {
+            const response = await axios.get(
+                userbaseUrl,
+            );
+            setExistingUsers(response.data);
+        }
+        catch (error) {
+        }
+    },[isFocused]); 
     return (
         <SafeAreaView style={styles.background}>
             <StatusBar style="auto"/>
-            <Text 
-                style= {styles.header}>
+            <Text style= {styles.header}>
                 Log in 
             </Text>
             <View style={styles.placeholderContainer}>
-            <TextInput
-                style = {styles.inputName}
-                placeholder = "Email"
-                mode="outlined"
-                theme={{roundness: 30, colors: {primary: 'black'}}}
-                value={email}
-                onChangeText={onChangeEmailHandler}
-                editable={!isLoading}
-                placeholderTextColor={"#908E8E"}
+                <TextInput
+                    style = {styles.inputName}
+                    placeholder = "Email"
+                    mode="outlined"
+                    theme={{roundness: 30, colors: {primary: 'black'}}}
+                    value={email}
+                    onChangeText={onChangeEmailHandler}
+                    editable={!isLoading}
+                    placeholderTextColor={"#908E8E"}
                />
             </View>
             <View style={styles.placeholderContainer}>
-            <TextInput
-                style = {styles.inputName}
-                placeholder = "Password"
-                value={password}
-                mode="outlined"
-                theme={{roundness: 30, colors: {primary: 'black'}}}
-                onChangeText={onChangePasswordHandler}
-                secureTextEntry={isSecurePassword}               
-                editable={!isLoading}
-                placeholderTextColor={"#908E8E"}
-                place
-                right={  
-                <TextInput.Icon onPress={() => { setIsSecurePassword((prev) => !prev)}} size={30} name={!isSecurePassword? "eye-outline" : "eye-off-outline"} />}
+                <TextInput
+                    style = {styles.inputName}
+                    placeholder = "Password"
+                    value={password}
+                    mode="outlined"
+                    theme={{roundness: 30, colors: {primary: 'black'}}}
+                    onChangeText={onChangePasswordHandler}
+                    secureTextEntry={isSecurePassword}               
+                    editable={!isLoading}
+                    placeholderTextColor={"#908E8E"}
+                    place
+                    right={  
+                        <TextInput.Icon onPress={() => { setIsSecurePassword((prev) => !prev)}} size={30} name={!isSecurePassword? "eye-outline" : "eye-off-outline"} />
+                    }
                 />
             </View>
-       
-         
             <View style={styles.loginWrap}>
                 <StandardButton sizeFont={20} title="Log in" functionOnPress={onSubmitFormHandler}/>
             </View> 
-            <Text
-                style = {styles.accountText}>
+            <Text style = {styles.accountText}>
                 Don't have an account yet?
             </Text>
             <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-                <Text style={styles.signUpButton}>Sign up</Text>
+                <Text style={styles.signUpButton}>
+                    Sign up
+                </Text>
             </TouchableOpacity>
         </SafeAreaView>
     );
@@ -128,27 +119,49 @@ function LogInScreen({navigation}) {
 
 export default LogInScreen;
 
-
 const styles = StyleSheet.create({
-    background: {
-        backgroundColor: '#7E9B6D',
-        flex: 1, 
-    }, 
-        
+    accountText: {
+        marginTop: 20,
+        marginLeft: 20, 
+        marginRight: 20,  
+        alignSelf: "center",
+        fontSize: 20,  
+    },
     arrowContainer: {
         height: 30, 
         width: 30, 
         marginLeft: 15,
         marginTop: 10,  
     },
-
+    background: {
+        backgroundColor: '#7E9B6D',
+        flex: 1, 
+    },     
     header: {
         marginLeft: 35, 
         fontSize: 35, 
         fontWeight: 'bold', 
         marginTop: 40, 
     },
-
+    icon: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputName: {
+        marginTop: -10, 
+        marginLeft: 30,
+        marginRight: 30,
+        borderRadius: 30,
+        height: 50,
+        width: 360,  
+    },
+    loginWrap:{
+        marginTop: 50,
+        alignSelf: 'center',
+        width: "60%",
+        height: 40,
+        margin: 10,
+    },
     placeholderContainer: {
         marginTop: 40, 
         marginLeft: 30,
@@ -158,40 +171,9 @@ const styles = StyleSheet.create({
         width: 360,
         alignItems: "center"  
     },
-
-    inputName: {
-        marginTop: -10, 
-        marginLeft: 30,
-        marginRight: 30,
-        borderRadius: 30,
-        height: 50,
-        width: 360,  
-    },
-
-    accountText: {
-        marginTop: 20,
-        marginLeft: 20, 
-        marginRight: 20,  
-        alignSelf: "center",
-        fontSize: 20,  
-    },
-
-    loginWrap:{
-        marginTop:50,
-        alignSelf: 'center',
-        width: "60%",
-        height: 40,
-        margin: 10,
-    },
-
     signUpButton: {
         textDecorationLine: 'underline',
         alignSelf: 'center',
         fontSize: 20,
-    },
-
-    icon: {
-        justifyContent: 'center',
-        alignItems: 'center',
     },
 })
